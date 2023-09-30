fa2b97d7a6a88fa6351274825f8df2c76cd3b2d2
pal6158 <pal61586@gmail.com>
2023-09-30 19:52:16 +0530

    reverse

---
+++ stringBuilder.java
@@ -0,0 +1,7 @@
+public class stringBuilder {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("Tony");
+        sb.append(" Stark");
+        System.out.println(sb);
+    }
+}
