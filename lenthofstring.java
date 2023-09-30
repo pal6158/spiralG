07274d6b9d2a26a6694ba47fd515e7eb9dc8eb82
pal6158 <pal61586@gmail.com>
2023-09-30 17:55:53 +0530

    replace

--- lenthofstring.java
+++ lenthofstring.java
@@ -1,21 +1,19 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class lenthofstring {
-   public static void main(String args[]) {
-     Scanner sc = new Scanner (System.in);
-     int size = sc.nextInt();
-     String array[] = new String[size];
-     int totLength = 0;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        String array[] = new String[size];
+        int totalLength = 0;
 
 
-     for(int i=0; i<size; i++) {
-       array[i] = sc.next();
-       totLength =totLength+ array[i].length();
-    //    System.out.println(totLength);
-     }
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.next();
+            totalLength = totalLength + array[i].length();
 
-
-     System.out.println(totLength);
-   }
+        }
+        System.out.println(totalLength);
+       
+    }
 }
